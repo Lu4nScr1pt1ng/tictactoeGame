1c4189c45c926b7a9e971b34e4f062025916e788
Lu4nScr1pt1ng <rd1qys@gmail.com>
2022-04-10 13:11:19 -0300

    menu principal e tabuleiro

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+const button2player = document.getElementById("vs-player");
+const button2ia = document.getElementById("vs-ia");
+
+
+
+button2player.onclick = function () {
+    document.querySelector('.apresentaJogo').className += ' hide';
+};
+
+button2ia.onclick = function () {
+    document.querySelector('.apresentaJogo').className += ' hide';
+};
+
